279d8ec02a14f52c956e1e7531a2be53aa153e4e
mosorio22 <moso4410@gmail.com>
2022-04-02 10:47:56 -0400

    Optimize dom manipulation on landing page

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,20 +1,28 @@
 //function to add users to page
 function addUsers(users) {
 	window.data = users;
+	const size = users.length;
 	//create new profile divs and append to wrapper div
-	for (let i = 0; i < users.length; i++) {
-		let newDiv = document.createElement("div");
-		newDiv.className = "user-profile";
-		let newHeader = document.createElement("a");
-		newHeader.className = "user-name";
-		newHeader.innerText = users[i]["name"]["first"] + " " + users[i]["name"]["last"];
-		newDiv.appendChild(newHeader);
-		let newAddress = document.createElement("p");
-		newAddress.className ="user-address";
-		newAddress.innerText = users[i]["location"]["street"]["number"] + " " + users[i]["location"]["street"]["name"];
-		newDiv.appendChild(newAddress);
-		document.getElementById("wrapper").appendChild(newDiv);
+	let newDiv = document.createElement("div");
+	newDiv.className = "user-profile";
+	let newAnchor = document.createElement("a");
+	newAnchor.className = "user-name";
+	let newAddress = document.createElement("p");
+	newAddress.className ="user-address";
+	let wrapper = document.getElementById("wrapper");
+	//store clones in document fragment to only append to dom once
+	let documentFragment = document.createDocumentFragment();
+	for (let i = 0; i < size; i++) {
+		let divClone = newDiv.cloneNode(true);
+		let anchorClone = newAnchor.cloneNode(true);
+		anchorClone.text = `${users[i]["name"]["first"]} ${users[i]["name"]["last"]}`;
+		divClone.appendChild(anchorClone);
+		let addressClone = newAddress.cloneNode(true);
+		addressClone.text = `${users[i]["location"]["street"]["number"]} ${users[i]["location"]["street"]["name"]}`;
+		divClone.appendChild(addressClone);
+		documentFragment.appendChild(divClone);
 	}
+	wrapper.appendChild(documentFragment);
 }
 
 //randomuser call with 50 users and seed "book" to always get same results
