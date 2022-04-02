f7fe6a80d047b966004a2763164d0115aaba2c4e
mosorio22 <moso4410@gmail.com>
2022-04-01 22:42:09 -0400

    small formatting edits. Install Jest but ultimately did not add test files

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,8 +1,15 @@
 function addUsers(users) {
-	console.log(users);
+	window.users = users;
 }
 
 fetch('https://randomuser.me/api/?results=50&seed=book')
-        .then(response => response.json())
-        .then(data => addUsers(data))
-        .catch(console.error("randomuser call failed"));
+	.then(response => response.json())
+    .then(data => addUsers(data["results"]))
+    .catch((error) => {
+		console.error(error)
+	});
+
+
+if (typeof exports !== 'undefined') {
+    module.exports.addUsers = addUsers;
+}
