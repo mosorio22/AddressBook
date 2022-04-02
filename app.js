92b5204ec6225c6d3136f7f60aaedf6b700e1425
mosorio22 <moso4410@gmail.com>
2022-04-01 23:13:18 -0400

    comment code and append user data to html

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+//set up js extensions
 app.use('*/js',express.static('public/js'));
+//index.html is landing page
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
+//localhost:3000
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
--- public/js/landing.js
+++ public/js/landing.js
@@ -1,15 +1,26 @@
+//function to add users to page
 function addUsers(users) {
-	window.users = users;
+	window.data = users;
+	//create new profile divs and append to wrapper div
+	for (let i = 0; i < users.length; i++) {
+		let newDiv = document.createElement("div");
+		newDiv.className = "user-profile";
+		let newHeader = document.createElement("a");
+		newHeader.className = "user-name";
+		newHeader.innerText = users[i]["name"]["first"] + " " + users[i]["name"]["last"];
+		newDiv.appendChild(newHeader);
+		let newAddress = document.createElement("p");
+		newAddress.className ="user-address";
+		newAddress.innerText = users[i]["location"]["street"]["number"] + " " + users[i]["location"]["street"]["name"];
+		newDiv.appendChild(newAddress);
+		document.getElementById("wrapper").appendChild(newDiv);
+	}
 }
 
+//randomuser call with 50 users and seed "book" to always get same results
 fetch('https://randomuser.me/api/?results=50&seed=book')
 	.then(response => response.json())
     .then(data => addUsers(data["results"]))
     .catch((error) => {
 		console.error(error)
 	});
-
-
-if (typeof exports !== 'undefined') {
-    module.exports.addUsers = addUsers;
-}
