ffbb9a5eb54a2cd2bb20fb0d61cec85a097ff2ac
mosorio22 <moso4410@gmail.com>
2022-04-08 17:58:37 -0400

    add more details to description page

--- public/js/description.js
+++ public/js/description.js
@@ -6,9 +6,13 @@ function populateProfile() {
 	let userName = params.username.split("|").join(" ");
 	let phoneNumber = params.phone;
 	let userImage = decodeURIComponent(params.pic);
+	let userAddress = params.address.split("|").join(" ");
+	let userAge = params.age;
 	document.querySelector(".profile-image").src = userImage;
 	document.querySelector(".profile-name").innerText = userName;
 	document.querySelector(".profile-phone").innerText = phoneNumber;
+	document.querySelector(".profile-age").innerText = userAge;
+	document.querySelector(".profile-address").innerText = userAddress;
 }
 
 populateProfile();
